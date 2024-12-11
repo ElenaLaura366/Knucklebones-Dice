2f267df04964de852fd7c2c64e58055bb624275f
Dutu Andrada Roxana Brindusa <andrada.dutu@student.unitbv.ro>
2024-12-11 16:19:08 +0200

    added soundtracks, music not playing yet

--- Knucklebones-Dice/UIKnucklebones/main.cpp
+++ Knucklebones-Dice/UIKnucklebones/main.cpp
@@ -3,31 +3,56 @@
 #include <QApplication>
 #include <QFontDatabase>
 #include <QIcon>
+#include <QMediaPlayer>
+#include <QAudioOutput>
+#include <QDebug>
+#include <qmessagebox.h>
 
+void startBackgroundMusic(QMediaPlayer* player, QAudioOutput* audioOutput) {
+    player->setAudioOutput(audioOutput);
+    player->setSource(QUrl(":/soundtracks/spongebobSong.wav"));
+    player->setLoops(QMediaPlayer::Infinite);
+    player->play();
+    audioOutput->setVolume(1.0);
 
-int main(int argc, char* argv[])
-{
-	QApplication app(argc, argv);
-	app.setWindowIcon(QIcon(":/images/icon.png"));
+    if (player->source().isEmpty()) {
+        qDebug() << "Fișierul audio nu a fost încărcat corect!";
+    }
+    else {
+        qDebug() << "Fișierul audio a fost încărcat!";
+    }
+	//sa ne atentioneze daca apare o eroare la redare
+    QObject::connect(player, &QMediaPlayer::errorOccurred, [](QMediaPlayer::Error error) {
+        QMessageBox::critical(nullptr, "Eroare la redare", QString("Eroare la redare audio: %1").arg(error));
+        });
+}
 
-	int fontId = QFontDatabase::addApplicationFont(":/fonts/SpenbebGame-R9q46.otf");
-	if (fontId == -1) {
-		QApplication::setFont(QFont("Comic Sans MS"));
-	}
-	else {
-		QString fontFamily = QFontDatabase::applicationFontFamilies(fontId).at(0);
-		QFont customFont(fontFamily, 14);
-		QApplication::setFont(customFont);
-	}
+int main(int argc, char* argv[]) {
+    QApplication app(argc, argv);
+    app.setWindowIcon(QIcon(":/images/icon.png"));
 
-	QFile styleFile(":/themes/spongebob_theme.qss");
-	if (styleFile.open(QFile::ReadOnly)) {
-		QString theme = styleFile.readAll();
-		app.setStyleSheet(theme);
-	}
+    QMediaPlayer player;
+    QAudioOutput audioOutput;
+    startBackgroundMusic(&player, &audioOutput);
 
-	MainWindow mainWindow(GameState("Player 1", "Player 2"));
-	mainWindow.show();
+    int fontId = QFontDatabase::addApplicationFont(":/fonts/SpenbebGame-R9q46.otf");
+    if (fontId == -1) {
+        QApplication::setFont(QFont("Comic Sans MS"));
+    }
+    else {
+        QString fontFamily = QFontDatabase::applicationFontFamilies(fontId).at(0);
+        QFont customFont(fontFamily, 14);
+        QApplication::setFont(customFont);
+    }
 
-	return app.exec();
+    QFile styleFile(":/themes/spongebob_theme.qss");
+    if (styleFile.open(QFile::ReadOnly)) {
+        QString theme = styleFile.readAll();
+        app.setStyleSheet(theme);
+    }
+
+    MainWindow mainWindow(GameState("Player 1", "Player 2"));
+    mainWindow.show();
+
+    return app.exec();
 }
