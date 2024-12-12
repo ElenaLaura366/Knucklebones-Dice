a7294283674936175aef383b6afcf660bba3d8c8
Dutu Andrada Roxana Brindusa <andrada.dutu@student.unitbv.ro>
2024-12-12 23:34:50 +0200

    changed start Widget +start Game

--- Knucklebones-Dice/UIKnucklebones/StartWidget.cpp
+++ Knucklebones-Dice/UIKnucklebones/StartWidget.cpp
@@ -1,44 +1,95 @@
-#include "StartWidget.h"
+﻿#include "StartWidget.h"
 
 #include "MainWindow.h"
 #include "GameWidget.h"
-
+#include <EasyDifficulty.h>
+#include <HardDifficulty.h>
 #include <QPropertyAnimation>
-
-
 StartWidget::StartWidget(Game&& game, MainWindow* parent)
-	: BaseMainWidget(parent)
-	, m_game(std::move(game))
+    : BaseMainWidget(parent)
+    , m_game(std::move(game))
+    , m_selectedMode("")
 {
-	GetParentWindow()->setWindowTitle("Start");
-	QPushButton* startButton = new QPushButton("Play", this);
-	startButton->setFixedSize(100, 50);
+    GetParentWindow()->showMaximized();
+    GetParentWindow()->setWindowTitle("Select Game Mode");
 
-	QBoxLayout* layout = new QHBoxLayout(this);
-	layout->addStretch(1);
-	layout->addWidget(startButton);
-	layout->addStretch(1);
+    m_selectMultiplayer = new QPushButton("Multiplayer", this);
+    m_selectPC = new QPushButton("Play vs PC", this);
+    m_selectEasy = new QPushButton("Easy", this);
+    m_selectHard = new QPushButton("Hard", this);
+    m_startButton = new QPushButton("Play", this);
 
-	setLayout(layout);
+    QSize buttonSize(150, 50); 
+    m_selectMultiplayer->setFixedSize(buttonSize);
+    m_selectPC->setFixedSize(buttonSize);
+    m_selectEasy->setFixedSize(buttonSize);
+    m_selectHard->setFixedSize(buttonSize);
+    m_startButton->setFixedSize(buttonSize);
 
-	connect(startButton, &QPushButton::clicked, this, &StartWidget::StartGame);
+    m_selectEasy->setEnabled(false);
+    m_selectHard->setEnabled(false);
+    m_startButton->setEnabled(false);
+
+    QVBoxLayout* mainLayout = new QVBoxLayout(this);
+    QLabel* instructionLabel = new QLabel("Select Game Mode:");
+    instructionLabel->setAlignment(Qt::AlignCenter);
+
+    mainLayout->addWidget(instructionLabel, 0, Qt::AlignCenter);
+    mainLayout->addWidget(m_selectMultiplayer, 0, Qt::AlignCenter);
+    mainLayout->addWidget(m_selectPC, 0, Qt::AlignCenter);
+
+    m_difficultyLabel = new QLabel("Select Difficulty:");
+    m_difficultyLabel->setAlignment(Qt::AlignCenter);
+    m_difficultyLabel->setVisible(false);
+    mainLayout->addWidget(m_difficultyLabel, 0, Qt::AlignCenter);
+    mainLayout->addWidget(m_selectEasy, 0, Qt::AlignCenter);
+    mainLayout->addWidget(m_selectHard, 0, Qt::AlignCenter);
+
+    mainLayout->addWidget(m_startButton, 0, Qt::AlignCenter);
+    setLayout(mainLayout);
+
+    connect(m_selectMultiplayer, &QPushButton::clicked, this, [this]() {
+        m_selectedMode = "Multiplayer";
+        m_selectEasy->setEnabled(false);
+        m_selectHard->setEnabled(false);
+        m_difficultyLabel->setVisible(false);
+        m_startButton->setEnabled(true);
+        });
+
+    connect(m_selectPC, &QPushButton::clicked, this, [this]() {
+        m_selectedMode = "";
+        m_selectEasy->setEnabled(true);
+        m_selectHard->setEnabled(true);
+        m_difficultyLabel->setVisible(true);
+        m_startButton->setEnabled(false);
+        });
+
+    connect(m_selectEasy, &QPushButton::clicked, this, [this]() {
+        m_selectedMode = "PC-Easy";
+        m_startButton->setEnabled(true);
+        });
+
+    connect(m_selectHard, &QPushButton::clicked, this, [this]() {
+        m_selectedMode = "PC-Hard";
+        m_startButton->setEnabled(true);
+        });
+
+    connect(m_startButton, &QPushButton::clicked, this, &StartWidget::StartGame);
 }
+
 
 void StartWidget::StartGame()
 {
-	/*QPropertyAnimation* animation = new QPropertyAnimation(this, "windowOpacity");
-	animation->setDuration(1000);
-	animation->setStartValue(1.0);
-	animation->setEndValue(0.0);
-	animation->start(QAbstractAnimation::DeleteWhenStopped);
+    std::unique_ptr<IOpponentDifficulty> difficulty;
+    if (m_selectedMode == "PC-Easy") {
+        difficulty = std::make_unique<EasyDifficulty>();
+    }
+    else if (m_selectedMode == "PC-Hard") {
+        difficulty = std::make_unique<HardDifficulty>();
+    }
 
-	connect(animation, &QPropertyAnimation::finished, this, [this]() {
-		GameWidget* gameWidget = new GameWidget(std::move(m_game), 20, GetParentWindow());
-		GetParentWindow()->setCentralWidget(gameWidget);
-		deleteLater();
-		});*/
-
-	GameWidget* gameWidget = new GameWidget(std::move(m_game), 3, GetParentWindow());
-	GetParentWindow()->setCentralWidget(gameWidget);
-	deleteLater();
+    Game game("Player 1", "Player 2", std::move(difficulty));
+    GameWidget* gameWidget = new GameWidget(std::move(game), 3, GetParentWindow());
+    GetParentWindow()->setCentralWidget(gameWidget);
+    deleteLater();
 }
--- Knucklebones-Dice/UIKnucklebones/StartWidget.h
+++ Knucklebones-Dice/UIKnucklebones/StartWidget.h
@@ -1,22 +1,33 @@
-#pragma once
+﻿#pragma once
 
 #include "BaseMainWidget.h"
 #include "Game.h"
 
 #include <QPushButton>
+#include <QLabel>
 #include <QMainWindow>
 
 
 class StartWidget : public BaseMainWidget
 {
-	Q_OBJECT
+    Q_OBJECT
 
 public:
-	StartWidget(Game&& game, MainWindow* parent);
+    StartWidget(Game&& game, MainWindow* parent);
 
 private slots:
-	void StartGame();
+    void StartGame();
 
 private:
-	Game m_game;
+    QPushButton* m_selectMultiplayer;
+    QPushButton* m_selectPC;
+    QPushButton* m_selectEasy;
+    QPushButton* m_selectHard;
+    QPushButton* m_startButton;
+
+    QLabel* m_difficultyLabel; 
+    QString m_selectedMode;
+
+private:
+    Game m_game;
 };
