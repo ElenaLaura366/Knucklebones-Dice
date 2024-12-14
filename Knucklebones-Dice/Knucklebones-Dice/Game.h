c1f1cea9f438984a41039d3ee525ce9585aa7741
Simona Grigoras <andreeasimona180@gmail.com>
2024-12-14 17:53:46 +0200

    added documentation for the Game class constructor and Make move from Game class

--- Knucklebones-Dice/Knucklebones-Dice/Game.h
+++ Knucklebones-Dice/Knucklebones-Dice/Game.h
@@ -6,10 +6,27 @@
 #include "Board.h"
 #include "Player.h"
 
-
+/*!
+	Class responsible of handling the game logic.
+*/
 class Game : public IObservable
 {
 public:
+	//! Game class constructor
+	/*! 
+	*	\brief Takes the player names, the difficulty, board size and maximum number to roll.
+	*	\param namePlayer1 sets the name of the first player.
+	*	\param namePlayer2 sets the name of the second player.
+	*	\param opponentDifficulty sets the difficulty of the game played.
+	*	If null the game will be set on multiplayer mode - 2 players, 
+	*	otherwise the computer will take the role of the second player.
+	*	\param boardRows sets the number of rows the board will have.
+	*	If not specified, it will be set on 3.
+	*	\param boardCols sets the number of columns the board will have.
+	*	If not specified, it will be set on 3.
+	*	\param maxValue sets the maximum value a palyer can roll.
+	*	If not specified, it will be set on 6.
+	*/
 	Game(std::string_view namePlayer1, std::string_view namePlayer2,
 		std::unique_ptr<IOpponentDifficulty> opponentDifficulty,
 		int boardRows = Board::DefaultRows, int boardCols = Board::DefaultCols, int maxValue = Game::DefaultMaxValue);
@@ -19,6 +36,19 @@ public:
 	Game& operator=(const Game&) = delete;
 	Game& operator=(Game&&) = delete;
 
+	//! Handles the move of a player/computer
+	/*! 
+	*	Takes as input the place of the move and make it, if it is valid.
+	*	\brief It will also switch the active player.
+	*	\brief If the game mode is set on singleplayer, (player vs computer) the computer will follow with its move.
+	*	\param col is the index of the column.
+	*	\param value is the value the player rolled.
+	*	\sa Board::MakeMove
+	*	\sa GetRandomValue
+	*	\sa EasyDifficulty::NextMove
+	*	\sa HardDifficulty::NextMove
+	*	\sa NotifyOnGameOver
+	*/
 	void MakeMove(int col, int value);
 
 	const Player& GetActivePlayer() const;
