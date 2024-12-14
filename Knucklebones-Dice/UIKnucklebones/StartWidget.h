fb59145a22be239bd9edb204314cf6d186b11687
macovei-alexandru <maccelmare@gmail.com>
2024-12-14 20:22:54 +0200

    documented UI except GameWidget

--- Knucklebones-Dice/UIKnucklebones/StartWidget.h
+++ Knucklebones-Dice/UIKnucklebones/StartWidget.h
@@ -10,6 +10,9 @@
 
 namespace ui
 {
+	/*!
+	*	\brief Widget that allows the user to select the game mode and difficulty.
+	*/
 	class StartWidget : public BaseMainWidget
 	{
 		Q_OBJECT
@@ -17,16 +20,23 @@ namespace ui
 	public:
 		StartWidget(MainWindow* parent);
 
-	private slots:
+	protected slots:
+		/*!
+		*	\brief Slot that sets the game mode to multiplayer.It does that by creating a new `ui::GameWidget`
+		*	and attaching it to the parent `ui::MainWindow`.
+		*/
 		void StartGame();
 
-	private:
+	protected:
+		/*!
+		*	\brief Enum representing the game mode currenty chosen.
+		*/
 		enum class GameMode
 		{
-			None,
-			Multiplayer,
-			PC_Easy,
-			PC_Hard
+			None,			/*!< No game mode selected */
+			Multiplayer,	/*!< Multiplayer mode selected */
+			PC_Easy,		/*!< Play vs PC on easy mode */
+			PC_Hard			/*!< Play vs PC on hard mode */
 		};
 
 	private:
